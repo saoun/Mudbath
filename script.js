fb1e94876a5d487511482d8ad382ffaf589ac2a3
sarah aoun <sarah@silk.co>
2016-07-23 14:26:20 -0400

    add instructions, enemy movement

--- script.js
+++ script.js
@@ -4,10 +4,21 @@
 
 //global variables
 var hero = $('<div class="hero">');
-$('body').append(hero);
+  $('body').append(hero);
 
-var box = $('<div class="box">')
-$('body').append(box);
+var mud = $('<div class="mud">')
+  $('body').append(mud);
+
+
+//instructions at beginning of game
+var instructions = $('<div class="instructions">');
+$('body').append(instructions);
+instructions.html("Use Arrow Keys to Move<br/>Space Bar to Attack")
+//remove instructions
+setTimeout(function(){
+  instructions.remove();
+}, 3500)
+
 
 //intervals for game check of functions
 setInterval(moveHero, 20);
@@ -16,6 +27,12 @@ setInterval(collision, 20);
 setInterval(death, 100);
 var keys = {}
 
+
+//moving hero
+//setting limits for up / down / left / right to be contained in background
+// maybe just delete up and down keys? to remove functionality
+// or figure out a jump option (to revisit)
+// http://stackoverflow.com/questions/7298507/move-element-with-keypress-multiple
 $(document).keydown(function(e) {
     keys[e.keyCode] = true;
 });
@@ -24,21 +41,18 @@ $(document).keyup(function(e) {
     delete keys[e.keyCode];
 });
 
-//moving hero
-//setting limits for up / down / left / right to be contained in background
-// maybe just delete up and down keys? to remove functionality
-// or figure out a jump option (to revisit)
-// http://stackoverflow.com/questions/7298507/move-element-with-keypress-multiple
 function moveHero() {
     for (var direction in keys) {
         if (!keys.hasOwnProperty(direction)) continue;
         if (direction == 37 && hero.position().left > -150) {
+            hero.addClass('left');
             hero.animate({left: "-=10"}, 0);
         }
         if (direction == 38 && hero.position().top > $(window).height()) {
             hero.animate({top: "-=10"}, 0);
         }
         if (direction == 39 && hero.position().left < $(window).width()) {
+            hero.removeClass('left');
             hero.animate({left: "+=10"}, 0);
         }
         if (direction == 40 && hero.position().top > $(window).height()) {
@@ -51,36 +65,130 @@ function moveHero() {
 function attack(){
   for (var direction in keys){
     if (!keys.hasOwnProperty(direction)) continue;
+
     if (direction == 32) {
-      hero.toggleClass('attack');
-      setTimeout (function(){
+      if (hero.hasClass('left')){
+        hero.toggleClass('attackleft')
+      } else {
         hero.toggleClass('attack');
+      }
+
+      setTimeout (function(){
+        if (hero.hasClass('left')){
+          hero.toggleClass('attackleft');
+        } else {
+          hero.toggleClass('attack');
+        }
       }, 100)
     }
   }
 }
 
-//collision detection
+//collision detection / enemy dies if hero attacks
 function collision() {
-  // determining positions of hero and box
-var heroPos = hero.offset().left + 488; //checks for width + 380
-var boxPos = box.offset().left;
+    // determining positions of hero and mud
+  var heroPos = hero.offset().left + 469; //checks for width + width of hero class
+  var mudPos = mud.offset().left;
 
-if (boxPos - heroPos < 0 && $('div').hasClass('attack') ){
-  console.log('hit');
-  box.remove();
-}
+  if (mudPos - heroPos < 0 && $('div').hasClass('attack') ){
+    console.log('hit');
+    mud.addClass('mudDeath');
+
+    setTimeout(function(){ //removes mud after death
+      mud.remove();
+    }, 100);
+
+    mud = $('.mud'); // this keeps checking for all mudes
+  }
 }
 
+//collision detection / hero dies if enemy touches them
 function death(){
-  var heroPos = hero.offset().left + 380; //checks for width + 380
-  var boxPos = box.offset().left;
+  var heroPos = hero.offset().left + 219; //checks for width + width of hero class
+  var mudPos = mud.offset().left;
 
-  if (boxPos - heroPos < 0  && boxPos){
-  console.log('die');
-  hero.remove()
+  if (mudPos - heroPos < 0 && mudPos ){
+  hero.remove();
+  console.log("GAME OVER");
 }
 }
+
+//creating enemy
+// function makeMud(){
+//   var mud = $('<div class="mud">')
+//   $('body').append(mud);
+// };
+
+// //needed to change the interval of setinterval thats running, got this code from:
+// // http://stackoverflow.com/questions/1280263/changing-the-interval-of-setinterval-while-its-running
+// var counter = 5000;
+
+// var moveMud = function(){
+
+//   $('.mud').animate({left: "-150"}, 4000); //1000 is how fast is move across the screen
+//   makeMud()
+
+//   if (counter>100){
+//     counter=counter-10; // how fast enemies appear
+//   }
+
+//   clearInterval(interval);
+//   interval = setInterval(moveMud, counter);
+// }
+// var interval = setInterval(moveMud, counter);
+
+
+
+
+
+
+
+/////////////////////
+
+// setInterval(moveBox, 20);
+
+// //moving the enemy across the screen DRAFT1
+// function moveBox(){
+
+//   setTimeout(function(){ //setting timeout to delay appearance of enemy
+//     box.animate({left: "-=5"}, 10);
+//   }, 1000);
+
+//   //remove box after it leaves screen
+//   if (box.position().left < 0){
+//     this.remove();
+//   }
+// }
+
+/////////////////////
+
+//DRAFT2: boxes appear and move across screen. decent option, no frequency increase though
+// function makeBox(){
+//   var box = $('<div class="box">')
+//   $('body').append(box);
+// };
+// function moveBox(){
+//   var box = $('.box');
+//   setInterval(function(){
+//   // var box = $('.box')
+//    box.animate({left: "-=5"}, 10);
+//   }, 1000)
+
+//   // remove box after it leaves screen
+//   if (box.position().left < 0){
+//     $(this).remove();
+//   }
+// }
+/////////////////////
+
+// create multiple enemies ???
+// for(var i=0; i<5; i++) {
+//     var box = $('<div class="box">')
+//     $('body').append(box);
+//     box.animate({left: "-=5"}, 20);
+
+//   }
+
 
 
 
--- script2.js
+++ script2.js
@@ -2,18 +2,49 @@
 (function(){
   console.log('loaded')
 
-// var canvas = $('#canvas');
-// ctx = canvas.getContext("2d");
 
-// canvas.width = 934;
-// canvas.height = 622
 
-// var background = new Image();
-// background.src = "img/background.jpeg";
+function makeBox(){
+  var box = $('<div class="box">')
+  $('body').append(box);
+};
 
-// background.onload = function(){
-//   ctx.drawImage(background,0,0);
+// var howOften = 100
+// function moveBox(){
+//   var box = $('.box');
+//   setInterval(function(){
+//   // var box = $('.box')
+//    box.animate({left: "-100"}, 1000); //1000 is how fast is move across the screen
+//    howOften = howOften / 2
+//   }, howOften)
+
+//   // remove box after it leaves screen
+//   if (box.position().left < 0){
+//     $(this).remove();
+//   }
 // }
+
+
+//needed to change the interval of setinterval thats running, got this code from:
+// http://stackoverflow.com/questions/1280263/changing-the-interval-of-setinterval-while-its-running
+var counter = 3000;
+
+// var boxmove = function(){
+//   $('.box').animate({left: "-100"}, 4000); //1000 is how fast is move across the screen
+//   makeBox()
+
+//   if (counter>100){
+//     counter=counter-10; // how fast enemies appear
+//   }
+
+//   clearInterval(interval);
+//   interval = setInterval(boxmove, counter);
+// }
+// var interval = setInterval(boxmove, counter);
+
+
+
+
 
 
 })();
